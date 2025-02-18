c89570800a2b4c4a2747f96e500f9a239606f42d
Charles Chadwick <charles@mutuallyassured.com>
2025-02-18 03:08:00 -0800

    Added trait

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -2,11 +2,19 @@
 
 namespace App\Enums;
 
+use App\Traits\EnumToArray;
+
 enum UserRole : string
 {
+    use EnumToArray;
+
     case SuperAdmin = "Super Admin";
     case Doctor = "Doctor";
     case Nurse = "Nurse";
     case Staff = "Staff";
     case Patient = "Patient";
+
+    public static function allStaff(): array {
+        return [self::Doctor, self::Nurse, self::Staff];
+    }
 }
