d72039c2aae0c94c8b4a73d174929cf6f7952d15
Charles Chadwick <charles@mutuallyassured.com>
2025-02-24 01:31:35 -0800

    Developed basic schedule seeder
    Just some basic schedule/appt seeding. No real checks for overlapping schedules or appts in here just yet.

--- database/seeders/ScheduleSeeder.php
+++ database/seeders/ScheduleSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Enums\AppointmentStatus;
 use App\Enums\UserRole;
+use App\Models\Appointment;
+use App\Models\Patient;
 use App\Models\Schedule;
 use App\Models\ScheduleUser;
 use App\Models\User;
@@ -20,7 +23,11 @@ class ScheduleSeeder extends Seeder
     {
 		DB::table('schedules')->delete();
 		DB::table('schedule_users')->delete();
+		DB::table('appointments')->delete();
+		
 		$last_date = Carbon::now()->nextWeekday();
+		
+		// create the date of the appt
         for($i = 1; $i <= 10; $i++) {
 			$date = fake()->dateTimeBetween($last_date, "{$i} week")->format('Y-m-d');
 			$schedule = Schedule::create([
@@ -32,6 +39,7 @@ class ScheduleSeeder extends Seeder
 				'created_by' => 1
 			]);
 			
+			// create the user
 			foreach(User::whereIn('role', UserRole::allStaff())->limit(rand(1, 3))->inRandomOrder()->get() as $user) {
 				ScheduleUser::create([
 					'schedule_id' => $schedule->id,
@@ -39,6 +47,24 @@ class ScheduleSeeder extends Seeder
 					'created_by' => 1
 				]);
 			};
+			
+			// now create random appts in that time frame fo the patient
+			for($appt_index = 0; $appt_index < rand(1, 3); $appt_index++) {
+				
+				$start = Carbon::parse($date.' 10:00')->addMinutes($appt_index * 15)->format('Y-m-d H:i:s');
+				$end = Carbon::parse($start)->addMinutes($appt_index + 15)->format('Y-m-d H:i:s');
+				
+				Appointment::create([
+					'schedule_id' => $schedule->id,
+					'patient_id' => User::where('role', UserRole::Patient)->get()->random()->id,
+					'status' => AppointmentStatus::Scheduled,
+					'start' => $start,
+					'end' => $end,
+					'title' => "Patient Appt",
+					'comments' => fake()->text(rand(5, 100)),
+					'created_by' => 1
+				]);
+			}
 			
 		}
     }
